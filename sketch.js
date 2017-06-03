b6b0d8e743861368feda16d923ce11f50f65dac2
Phil Turner <julietius@gmail.com>
2017-06-03 20:24:35 +0100

    Function updates to make fuller game
    Added particles that get destroyed and contrain to screen, along with a game over trigger

--- sketch.js
+++ sketch.js
@@ -1,46 +1,81 @@
 //var direction = 90; //circle initial direction moving down
 var player;
-var blobs = [];
-var TOTAL_BLOBS = 10;
+var particles;
+var timer;
+var MAX_PARTICLES = 10;
+var PART_SIZE = 50;
+var MARGIN = 40;
+var START_OFFSET = 750;
 
-function setup() {
+function setup(){
   createCanvas(800,600);
-  for (var i = 0; i < TOTAL_BLOBS; i++){
-    blobs[i] = createSprite(random(0,width), random(0,height), 10, 10);
-    blobs[i].setSpeed(3, random(0,10));
+  particles = new Group();
+
+  for (var i = 0; i < MAX_PARTICLES; i++){
+    var ang = random(360);
+    var px = width/2 + START_OFFSET  * cos(radians(ang));
+    var py = height/2+ START_OFFSET * sin(radians(ang));
+    createParticle(3, px, py);
   }
-  player = createSprite(random(0,width), random(0,height), 30, 125);
+  player = createSprite(random(0,width), random(0,height), 30, 100);
 }
 
-function draw() {
+function draw(){
   background(61);
+  //text at top of screen
+  fill(255);
+  textAlign(CENTER);
+  text("Use your mouse to destroy the blocks", width/2, 20);
+
+  //check to see if game overlap
+  if (particles.length == 0) gameOverMessage();
+
+  //check for sprites moving off screen and reposition
+  for(var i=0; i<allSprites.length; i++) {
+    var s = allSprites[i];
+      if(s.position.x<-MARGIN) s.position.x = width+MARGIN;
+      if(s.position.x>width+MARGIN) s.position.x = -MARGIN;
+      if(s.position.y<-MARGIN) s.position.y = height+MARGIN;
+      if(s.position.y>height+MARGIN) s.position.y = -MARGIN;
+  }
 
   //checks for overlap and removes sprite
-  for (var i = 0; i < blobs.length; i++){
-    if(player.overlap(blobs[i]))
-      blobs[i].remove();
+  for (var i = 0; i < particles.length; i++){
+    if(player.overlap(particles[i]))
+      particles[i].remove();
   }
 
-  //aside of setting the velocity directly you can move a sprite
-  //by providing a speed and an angle
-  //direction += 2;
-
-  for (var i = 0; i < blobs.length; i++){
-    //speed, angle
-    blobs[i].setSpeed(3)
-    if(blobs[i].x < 0 || blobs[i].x > width)
-      blobs[i].setDirection(45);
-  }
-
-  //or by applying a force toward a point
-  //force (acceleration), pointx, pointy
   player.attractionPoint(.2, mouseX, mouseY);
-
-  //since the force keeps incrementing the speed you can
-  //set a limit to it with maxSpeed
   player.maxSpeed = 4;
 
   //draw all sprites
   drawSprites();
+}
 
+function createParticle(type, x, y){
+  var p = createSprite(x, y, PART_SIZE, PART_SIZE);
+  //var img  = loadImage("assets/particle"+floor(random(0,3))+".png");
+  //a.addImage(img);
+  p.setSpeed(4.5-(type/2), random(360));
+  //a.rotationSpeed = .5;
+  //a.debug = true;
+  p.type = type;
+
+  if(type == 2)
+    p.scale = .6;
+  if(type == 1)
+    p.scale = .3;
+
+  p.mass =  2 + p.scale;
+  //a.setCollider("circle", 0, 0, 50);
+  particles.add(p);
+  return p;
+}
+
+function gameOverMessage(){
+  //console.log('Game Over');
+  fill(255);
+  textAlign(CENTER);
+  textSize(45);
+  text("You destroyed all the blocks #WIN", width/2, height/2);
 }
