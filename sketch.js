14afab2bcdd28267f2c7c86d50fce24281cab6b5
Phil Turner <julietius@gmail.com>
2017-06-03 21:31:55 +0100

    Updated variable naming
    To be more readable and match games theme

--- sketch.js
+++ sketch.js
@@ -1,22 +1,21 @@
 //var direction = 90; //circle initial direction moving down
 var player;
-var particles;
+var acorns;
 var timer = 0;
 var theTimer;
-var MAX_PARTICLES = 10;
-var PART_SIZE = 50;
+var MAX_ACORNS = 10;
 var MARGIN = 40;
 var START_OFFSET = 750;
 
 function setup(){
   createCanvas(800,600);
-  particles = new Group();
+  acorns = new Group();
 
-  for (var i = 0; i < MAX_PARTICLES; i++){
+  for (var i = 0; i < MAX_ACORNS; i++){
     var ang = random(360);
     var px = width/2 + START_OFFSET  * cos(radians(ang));
     var py = height/2+ START_OFFSET * sin(radians(ang));
-    createParticle(3, px, py);
+    createAcorn(3, px, py);
   }
   player = createSprite(random(0,width), random(0,height));
   var img  = loadImage("assets/squizza_sprite.png");
@@ -31,10 +30,10 @@ function draw(){
   //text at top of screen
   fill(255);
   textAlign(CENTER);
-  text("Use your mouse to move Squizza", width/2, 20);
+  text("Move your mouse to move Squizza", width/2, 20);
   text(timer + ' seconds', width/2, height-20);
   //check to see if game overlap
-  if (particles.length == 0) gameOverMessage();
+  if (acorns.length == 0) gameOverMessage();
 
   //check for sprites moving off screen and reposition
   for(var i=0; i<allSprites.length; i++) {
@@ -46,9 +45,9 @@ function draw(){
   }
 
   //checks for overlap and removes sprite
-  for (var i = 0; i < particles.length; i++){
-    if(player.overlap(particles[i]))
-      particles[i].remove();
+  for (var i = 0; i < acorns.length; i++){
+    if(player.overlap(acorns[i]))
+      acorns[i].remove();
   }
 
   player.attractionPoint(.2, mouseX, mouseY);
@@ -58,26 +57,26 @@ function draw(){
   drawSprites();
 }
 
-function createParticle(type, x, y){
-  var p = createSprite(x, y, PART_SIZE, PART_SIZE);
+function createAcorn(type, x, y){
+  var a = createSprite(x, y);
   var img  = loadImage("assets/acorn.png");
-  p.addImage(img);
-  p.setSpeed(4.5-(type/2), random(360));
-  p.rotationSpeed = .5;
+  a.addImage(img);
+  a.setSpeed(4.5-(type/2), random(360));
+  a.rotationSpeed = .5;
   //a.debug = true;
-  p.type = type;
+  a.type = type;
 
   if(type == 3)
-    p.scale = .05;
+    a.scale = .05;
   if(type == 2)
-    p.scale = .6;
+    a.scale = .6;
   if(type == 1)
-    p.scale = .3;
+    a.scale = .3;
 
-  p.mass =  2 + p.scale;
+  a.mass =  2 + a.scale;
   //a.setCollider("circle", 0, 0, 50);
-  particles.add(p);
-  return p;
+  acorns.add(a);
+  return a;
 }
 
 function gameOverMessage(){
